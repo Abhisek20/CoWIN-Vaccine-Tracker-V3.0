54492c0ad4b1e7049a104ed0ec61e0dc1f00e236
Abhisek20 <69687280+Abhisek20@users.noreply.github.com>
2021-06-03 21:28:35 +0530

    Add files via upload

--- CoWIN_V3.0.py
+++ CoWIN_V3.0.py
@@ -17,10 +17,14 @@ from playsound import playsound
 from datetime import datetime
 import time
 import webbrowser
+#import pyqrcode
+#import png
+#from pyqrcode import QRCode
+#from notify_run import Notify
 
 
 # In[12]:
-
+#31/313
 
 t1 = datetime.now()
 
@@ -45,28 +49,34 @@ version = config_data['version_name']
 app_type = config_data["app_type"]
 vaccine_limit = int(config_data["vaccine_lim"])
 time_limit_secs = int(config_data["time_limit_secs"])
-
+count = 0
 
 # In[18]:
 
 
 print("Covid Vaccine Tracker Console Version-{}".format(version), "\n")
 date = input('\nEnter Date(DD/MM/YYYY format only) => ')
-date = int(date.replace('/', ''))
+date = (date.replace('/', ''))
 dist_code = input("\nEnter District code => ")
 age = int(input('\nEnter age => '))
 
 
-# In[15]:
-
-
+# In[15]
+print(date)
 URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}".format(
     dist_code, date)
 header = {
     'User-Agent':
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'
         }
-
+#ny = Notify()
+#ny.register()
+#ep = (ny.endpoint)
+#qr = pyqrcode.create(ep)
+#qr.show()
+#now = datetime.now()
+#now = now.strftime("%d-%m-%Y %H:%M:%S")
+#qr.png(now+".png", scale = 8)
 
 # # Manual
 
@@ -77,8 +87,9 @@ if app_type == "manual":
     res = requests.get(URL, headers=header)
     response_json = res.json()
     response_df = pd.DataFrame(dict(response_json)['sessions'])
+    
     filter_df = response_df[(response_df.min_age_limit == age) & (response_df.available_capacity > vaccine_limit)]
-
+    
     from tkinter import *
 
     if filter_df.shape[0] > 0:
@@ -111,12 +122,16 @@ if app_type == "manual":
 
 elif app_type == "auto":
     while True:
+        count+=1
+        now = datetime.now()
+        now = now.strftime("%d-%m-%Y %H:%M:%S")
+        print("\nSearching for vaccine(Dose 1), trial number :{} at {}".format(count, now))
         response = requests.get(URL, headers=header)
         if response.ok:
             response_json = response.json()
             response_df = pd.DataFrame(dict(response_json)['sessions'])
             filter_df = response_df[(response_df.min_age_limit == age)
-                                    & (response_df.available_capacity > vaccine_limit)]
+                                    & (response_df.available_capacity_dose1 > vaccine_limit)]
             from tkinter import *
 
             if filter_df.shape[0] > 0:
@@ -130,11 +145,13 @@ elif app_type == "auto":
                     playsound('Alarm.mp3')
                 webbrowser.open("https://selfregistration.cowin.gov.in/", new=1)
                 for i in range(filter_df.shape[0]):
-                    Data = "Centre Name : {}, Address : {}, Pin :{}, Total dose :{}, Vaccine Type :{}".format(
+                    Data = "Time {}, Centre Name : {}, Address : {}, Pin :{}, Total dose :{}, Vaccine Type :{}".format(now,
                         filter_df.name.iloc[i], filter_df.address.iloc[i],
                         filter_df.pincode.iloc[i],
                         filter_df.available_capacity.iloc[i],
                         filter_df.vaccine.iloc[i])
+                    print("Vaccine found in: {}".format(Data))
+                    #ny.send("Vaccine found in: {}".format(Data))
                     mylist.insert(END, Data)
 
                 mylist.pack(side=LEFT, fill=BOTH)
